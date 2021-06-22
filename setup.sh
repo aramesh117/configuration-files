b7d64d04f1e5eb4eeb89b7e076d7080879c6ff54
Aaditya Ramesh <aaditya_ramesh@apple.com>
2021-06-22 14:01:34 -0700

    [setup/vim] Script to do initial setup
    Symlink and then install plugins for vim

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+function setup_vim {
+  mkdir -p ~/.config/nvim/
+  ln -fs "$SCRIPT_DIR/.config/nvim/init.vim"
+  nvim +PluginInstall +qall
+  rm init.vim
+}
+
 ln -fs "$SCRIPT_DIR/.aliases" ~/.aliases
 ln -fs "$SCRIPT_DIR/.zshrc" ~/.zshrc
 ln -fs "$SCRIPT_DIR/.vars" ~/.vars
@@ -10,3 +17,6 @@ ln -fs "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
 ln -fs "$SCRIPT_DIR/.gitconfig" ~/.gitconfig
 ln -fs "$SCRIPT_DIR/.gitmessage" ~/.gitmessage
 ln -fs "$SCRIPT_DIR/.vimrc" ~/.vimrc
+
+setup_vim
+
